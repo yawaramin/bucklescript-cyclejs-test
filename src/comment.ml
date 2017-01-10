38aa6a90a3e18564010216e5a9431800dcfa3e43
Yawar Amin <yawar.amin@gmail.com>
2017-01-10 00:20:21 -0500

    Tweak basic comment layout

--- src/comment.ml
+++ src/comment.ml
@@ -16,21 +16,20 @@ let reply_to t = t.reply_to
 let timestamp t = t.timestamp
 let author t = t.author
 let msg t = t.msg
+let append_colon string = string ^ ": "
 let view t =
   let open Cycle_dom in
   let comment_id = t |> id |> string_of_int in
 
-  h "div.box" ["key", comment_id] [
-    h "article.media" [] [
-      h "div.media-content" [] [
-        h "div.content" [] [
-          h "strong" [] [t |> author |> text];
-          text " - 2017-01-01T16:01Z -";
-          t |> msg |> text ];
+  h "li.box" ["key", comment_id] [
+    h "p.block" [] [
+      text "2017-01-01T16:01Z ";
+      h "strong" [] [t |> author |> append_colon |> text];
+      t |> msg |> text ];
 
-        h "nav.level" [] [
-          h "div.level-left" [] [
-            h ("a#reply-" ^ comment_id ^ ".button.level-left")
-              []
-              [text "Reply"] ] ] ] ] ]
+    h "p.control.has-addons" [] [
+      h ("a#up-" ^ comment_id ^ ".button.up") [] [text "+1"];
+      h ("a#reply-" ^ comment_id ^ ".button.reply") [] [text "Reply"];
+      h ("a#down-" ^ comment_id ^ ".button.is-danger.down") [] [
+        text "-1"] ] ]
 
