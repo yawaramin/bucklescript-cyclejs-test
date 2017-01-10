4b5696ab3637ba7ea1e642bd944ebda3bb4d760a
Yawar Amin <yawar.amin@gmail.com>
2017-01-10 13:46:37 -0500

    Box up comments thread

--- src/comments.ml
+++ src/comments.ml
@@ -14,5 +14,6 @@ let reply id author msg t =
 
 let init_comment = start Comment.init_id "bob" "Test comment"
 
-let view t = t |> comment |> Comment.view
+let view t =
+  Cycle_dom.(h "ul.box" [h "li.box" [t |> comment |> Comment.view]])
 
